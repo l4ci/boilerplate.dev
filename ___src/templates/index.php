18ad6c99f1132500b01d4b328633de8dc65bbdc2
David Hellmann <davidhellmann.com@gmail.com>
2015-09-30 14:14:08 +0200

    Update Font Stuff

--- ___src/templates/index.php
+++ ___src/templates/index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" lang="de">
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -101,7 +101,7 @@
   ?>
   <!-- BrowserSync -->
   <script type='text/javascript' id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.9.6.js'><\/script>".replace("HOST", location.hostname));
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.9.7.js'><\/script>".replace("HOST", location.hostname));
 //]]></script>
 
   <?php } ?>
